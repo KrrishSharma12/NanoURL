313d4e71a10f32ea2b8982cfcc57507e320683a3
Krish Sharma <krishsharma1842@gmail.com>
2026-05-10 23:53:16 +0530

    Fix formatting of cookieOptions in cookie.config.js

--- Backend/src/config/cookie.config.js
+++ Backend/src/config/cookie.config.js
@@ -1,8 +1,7 @@
-export const cookieOptions={
-           httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    maxAge: 30 * 60 * 1000,
-    sameSite: 'lax',
-             path: "/"
-        }
+export const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 30 * 60 * 1000,
+  path: "/"
+};
